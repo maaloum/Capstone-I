b57adf660556fc6a2f0dd0732794001f751084f6
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-08-01 19:14:46 +0300

    add speakers with js

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -27,4 +27,74 @@ menu.addEventListener('click', ()=>{
 
 // Poping Speaker Section with JS
 
+const speakers =[
+{
+  img: "/Assets/speaker_01.png",
+  name: 'Weekend',
+  job: 'Musicien and Artist from USA',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
 
+},
+{
+  img: "/Assets/speaker_02.png",
+  name: 'Hamzo Bryn',
+  job: 'Musicien and Artist from Mauritania',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
+
+
+},
+{
+  img: "/Assets/speaker_03.png",
+  name: 'Mona Dendeni',
+  job: 'Musicien and Artist from Mauritania',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
+
+},
+{
+  img: "/Assets/speaker_04.png",
+  name: 'Nejwa Karam',
+  job: 'Musicien and Artist from Lebenon',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
+
+},
+{
+  img: "/Assets/speaker_05.png",
+  name: 'Kadem Saher',
+  job: 'Musicien and Artist from IRAQ',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
+
+},
+{
+  img: "/Assets/speaker_06.png",
+  name: 'Drek',
+  job: 'Musicien and Artist from USA',
+  description: 'Emerging young conductor and Winner of the Respighi Prize 2017 in Conducting, Jonathan Taylor Rush will bring passion, unique interpretation and refreshing energy to AMC.'
+
+}
+
+
+
+]
+
+const speakerSection = document.querySelector('#speakers');
+
+speakers.forEach((speaker, id)=>{
+  const speakereCard = document.createElement('div');
+  speakereCard.innerHTML = (`
+  <div class="speakers">
+    <div class="each speaker${id}">
+      <img src="${speaker.img}" alt="speaker${id}">
+    <div class = "speaker-info">
+      <h3 class="name">${speaker.name}</h3>
+      <p class="job">${speaker.job}</p>
+      <span class ="separation"></span>
+      <p class="desc">${speaker.description}</p>
+    <div/>
+    </div>
+  </div>
+
+  `);
+  speakereCard.classList.add('speakers-sec');
+  speakerSection.appendChild(speakereCard);
+
+})
